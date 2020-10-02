1cf03b905c6c109e89cc52505cfce0ff3924588c
Oscar Lopez H <oscar.lopezh@softtek.com>
2020-10-01 19:04:16 -0500

    crud super basico osea no?

---
+++ src/main/java/com/pruebas/rest/PersonaRest.java
@@ -0,0 +1,39 @@
+package com.pruebas.rest;
+
+import com.pruebas.dao.PersonaDAO;
+import com.pruebas.model.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("personas")
+public class PersonaRest {
+
+    @Autowired
+    private PersonaDAO personaDAO;
+
+    //Metodos http
+
+    @PostMapping("/guardar")
+    public void guardar(@RequestBody Persona persona){
+        personaDAO.save(persona);
+    }
+
+    @GetMapping("/listar")
+    public List<Persona> listar(){
+        return personaDAO.findAll();
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        personaDAO.deleteById(id);
+    }
+
+    @PutMapping("/actualizar")
+    public void actualizar(@RequestBody Persona persona){
+        personaDAO.save(persona);
+    }
+
+}
